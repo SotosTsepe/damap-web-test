1d3c04ef9a8bbfa6e673ac772f3affd69075a1f4
Sotiris Tsepelakis <stsepelakis@gmail.com>
2022-05-25 14:26:15 +0200

    Update copyright info in files

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
 /*!
+* Damap website (https://damap.org)
+* Copyright 2022 TU Wien
+* Licensed under MIT (https://github.com/SotosTsepe/damap-website/blob/main/LICENSE)
+*/
+/*!
 * Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
