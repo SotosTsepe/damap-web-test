56a7c279abf82cb0513ced6c142a4ed64d58343d
Sotiris Tsepelakis <stsepelakis@gmail.com>
2022-10-19 16:51:10 +0200

    Remove cookie consent banner
    Obsolete as per 7843904c4a5a3b4809a1df69b9fe6b2acf7f5951

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,6 @@
 * Licensed under MIT (https://github.com/SotosTsepe/damap-website/blob/main/LICENSE)
 */
 /*!
-* Bootstrap Cookie Alert by Wruczek
-* https://github.com/Wruczek/Bootstrap-Cookie-Alert
-* Released under MIT license
-*/
-/*!
 * Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
@@ -114,57 +109,3 @@ function enableSubmit() {
     }
     btn.disabled = !isValid;
 }
-
-
-// cookie consent
-(function () {
-  "use strict";
-
-  var cookieAlert = document.querySelector(".cookiealert");
-  var acceptCookies = document.querySelector(".acceptcookies");
-
-  if (!cookieAlert) {
-    return;
-  }
-
-  cookieAlert.offsetHeight; // Force browser to trigger reflow (https://stackoverflow.com/a/39451131)
-
-  // Show the alert if we cant find the "acceptCookies" cookie
-  if (!getCookie("acceptCookies")) {
-    cookieAlert.classList.add("show");
-  }
-
-  // When clicking on the agree button, create a 1 year
-  // cookie to remember user's choice and close the banner
-  acceptCookies.addEventListener("click", function () {
-    setCookie("acceptCookies", true, 365);
-    cookieAlert.classList.remove("show");
-
-    // dispatch the accept event
-    window.dispatchEvent(new Event("cookieAlertAccept"))
-  });
-
-  // Cookie functions from w3schools
-  function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
-
-  function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) === ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) === 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
-})();
